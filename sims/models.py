e89b275f03e0f5a5dc4bf148e34254a4e7a7d216
John Huddleston <huddlej@gmail.com>
2014-08-23 21:04:50 -0700

    added simplest possible models for logging samples in pipelines.

--- sims/models.py
+++ sims/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Sample(models.Model):
+    """
+    Represents a single individual.
+    """
+    name = models.CharField(max_length=30, unique=True)
+
+
+class Pipeline(models.Model):
+    """
+    Represents a pipeline with one or more steps through which one or more
+    samples can be passed as input.
+    """
+    name = models.CharField(max_length=100)
+    version = models.CharField(max_length=10)
+
+
+class LogEntry(models.Model):
+    """
+    Represents a checkpoint during a specific pipeline for a given sample.
+    """
+    # TODO: implement sample as a GenericForeignKey?
+    sample = models.ForeignKey(Sample)
+    pipeline = models.ForeignKey(Pipeline)
+    # TODO: add model for rule tied to Pipeline and only log rule?
+    rule = models.CharField(max_length=100)
+    timestamp = models.DateTimeField()
+    status = models.CharField(max_length=10)
