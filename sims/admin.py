1ec546cbb27760f49346d85c8e405c71ffc1a69e
John Huddleston <huddlej@gmail.com>
2014-08-30 15:59:38 -0700

    setup rich interface for browsing log entries.

--- sims/admin.py
+++ sims/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from sims.models import LogEntry, Pipeline, Sample
 
-admin.site.register(LogEntry)
 admin.site.register(Pipeline)
 admin.site.register(Sample)
+
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ("sample", "pipeline", "rule", "timestamp", "status")
+    list_filter = ("pipeline__name", "status")
+
+admin.site.register(LogEntry, LogEntryAdmin)
